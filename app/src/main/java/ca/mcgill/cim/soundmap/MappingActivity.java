f55b708e95bf29de185aaf1d85d38ced8383cda7
Jeremy <jgomallette@gmail.com>
2018-06-19 03:17:55 -0400

    Add main button overlay

--- app/src/main/java/ca/mcgill/cim/soundmap/MappingActivity.java
+++ app/src/main/java/ca/mcgill/cim/soundmap/MappingActivity.java
@@ -1,46 +1,137 @@
 package ca.mcgill.cim.soundmap;
 
+//import android.*;
+import android.Manifest;
+import android.app.ActionBar;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+//import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+//import com.google.android.gms.maps.model.LatLng;
+//import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MappingActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MappingActivity extends FragmentActivity {
 
+    private static final String TAG = "MappingActivity";
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    private static final int PERMISSIONS_REQ_CODE = 5029;
+
+    private static String[] PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION,
+                                           Manifest.permission.RECORD_AUDIO};
+
+    private ActionBar mActionBar;
+    private boolean mPermissionGranted = false;
+    private boolean mMapInitiated = false;
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapping);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        mActionBar = getActionBar();
+//        mActionBar.setDisplayUseLogoEnabled(true);
+
+        // TODO : Pull to an earlier screen whenever it is created
+        if (isServicesAvailable()) {
+            getPermissions();
+            if (mPermissionGranted && !mMapInitiated) {
+                initMap();
+            }
+        }
     }
 
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+            }
+        });
+    }
+
+    private void getPermissions() {
+        boolean denied = false;
+
+        // Iterate through permissions,
+        // If any are not granted, set denied flag to true
+        for (String permission : PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    permission) != PackageManager.PERMISSION_GRANTED) {
+                denied = true;
+                break;
+            }
+        }
+
+        if (denied) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSIONS_REQ_CODE);
+        } else {
+            mPermissionGranted = true;
+        }
+    }
+
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (requestCode == PERMISSIONS_REQ_CODE) {
+            boolean denied = false;
+
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    denied = true;
+                    break;
+                }
+            }
+
+            if (denied) {
+                Toast.makeText(this, "Permissions were not granted. The app will not proceed",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                mPermissionGranted = true;
+                if (!mMapInitiated) {
+                    initMap();
+                }
+            }
+        }
+    }
+
+    private boolean isServicesAvailable() {
+        Log.d(TAG, "Verifying Google Play Services version and connectivity...");
+        int available = GoogleApiAvailability.getInstance()
+                .isGooglePlayServicesAvailable(MappingActivity.this);
+
+        if (available == ConnectionResult.SUCCESS) {
+            Log.d(TAG, "Google Play Services successfully connected.");
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            Log.d(TAG, "An error occurred, but can be resolved by the user");
+            Dialog dialog = GoogleApiAvailability.getInstance()
+                    .getErrorDialog(MappingActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+            return false;
+        } else {
+            Log.d(TAG, "An unresolvable error occurred with Google Play Services");
+            Toast.makeText(this, "An unresolvable error occurred with Google Play Services",
+                    Toast.LENGTH_LONG).show();
+            return false;
+        }
     }
 }
